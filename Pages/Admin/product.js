f5690bb7abb272375489fdcfdd130c91312e53bf
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-02-02 04:30:27 +0700

    done product

--- Pages/Admin/product.js
+++ Pages/Admin/product.js
@@ -14,17 +14,37 @@ function show() {
     html += '<td>'+'<p>' + Product[i].id + '</p>'+"</td>";
   //   html+='<td>'+'<p>17th Oct, 15</p>'+'<p class="text-danger">Overdue</p>'+'</td>'
     html += '<td>' + Product[i].name + "</td>";
-    html +='<td class="member"> <figure><img src="' +Product[i].img +'"/></figure><div class="member-info"><p>'+Product[i].name+'</p><p>'+Product[i].desc+'</p></div></td>';
+    if (Product[i].img.indexOf("http"))
+    {
+      html +='<td class="member"> <figure><img src="../../asset/picture/'+Product[i].img+'"/></figure></div></td>';
+    }
+    else{
+     
+      html +='<td class="member"> <figure><img src="' +Product[i].img +'"/></figure></div></td>';
+    }
+    html += '<td><div class="member-info"><p>' + Product[i].desc + "</p></div></td>";
+
+    // <div class="member-info"><p>'+Product[i].name+'</p><p>'+Product[i].desc+'</p>
     html += '<td><p>' + Product[i].price + '</p>'+"</td>";
-    html += '<td class="status">' + '<span class="status-text status-orange">In progress</span>'+ "</td>";
-
-
+    html += '<td >' + Product[i].promotion + "</td>";
+    if(Product[i].status==true){
+      html += '<td >' + "active" + "</td>";
+    }
+    else{
+      html += '<td >' + "block" + "</td>";}
     html +='<td>';
-    html+=`<ul><li class='nav-item dropdown '>`
+    html+=`<ul><li class='nav-item dropdown tdCenter '>`
     html+='<a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action</a>'
     html+='<div class="dropdown-menu" aria-labelledby="navbarDropdown">'
     html+=`<button onClick="deletes(${Product[i].id})" class="dropdown-item" >DELETE</button>`
     html+=`<button onClick="edit(${Product[i].id})" data-toggle="modal" data-target="#exampleModal" class="dropdown-item" >UPDATE</button>`
+    if(Product[i].status==true){
+      html+=`<button onClick="block(${Product[i].id})" class="dropdown-item" >BLOCK</button>`
+    }
+    else{
+      html+=`<button onClick="active(${Product[i].id})" class="dropdown-item" >ACTIVE</button>`
+
+    }
 
     
 
@@ -57,15 +77,21 @@ var name = document.getElementById("name").value;
 var detail = document.getElementById("desc").value;
 var category = document.getElementById("category").value;
 var price = document.getElementById("price").value;
+var img=document.getElementById("fileInput").value;
+let image = img.split("\\")[2];
+var promotion=document.getElementById("promotion").value;
 
-console.log(name,detail,category,price);
-// var img = document.getElementById("name").value;
+console.log(image);
+
 
 var OneProduct = {
   name: name,
   desc:detail,
   price: price,
-  type:category
+  type:category,
+  img:image,
+  status:true,
+  promotion:promotion
 };
 console.log(OneProduct);
 axios.post('https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products', OneProduct)
@@ -76,6 +102,7 @@ axios.post('https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products', OneProduct)
   console.log(error);
 });
 alert("Done!");
+window.location.href="product.html";
 
 }
 
@@ -113,6 +140,16 @@ axios.get('https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products')
           document.getElementById("desc").value=product[i].desc;
           document.getElementById("category").value=product[i].type;
           document.getElementById("price").value=product[i].price;
+          document.getElementById("promotion").value=product[i].promotion;
+          if (product[i].img.indexOf("http"))
+          {
+            document.getElementById("image").innerHTML='<img  src="../../asset/picture/'+product[i].img+'"/>';
+          }
+          else{
+           
+            document.getElementById("image").innerHTML='<img src="' +product[i].img +'"/>';
+          }
+          document.getElementById("status").value=product[i].status;
         }
   }
   
@@ -123,21 +160,60 @@ axios.get('https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products')
 
 
 }
+function block(id){
+
+        var OneProduct = {
+          status:false,
+      }
+      console.log(OneProduct);
+  axios.put(`https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products/${id}`, OneProduct)
+    .then(function (response) {
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    alert("Done!");
+    setTimeout(() => {
+      window.location.reload(false);
+    }, 1000);
+    }
 
 
+function active(id){
+  
+        var OneProduct = {
+          status:true,
+      }
+      console.log(OneProduct);
+  axios.put(`https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products/${id}`, OneProduct)
+    .then(function (response) {
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    alert("Done!");
+    setTimeout(() => {
+      window.location.reload(false);
+    }, 1000);
+    }
+    
 function editt() {
   var id = document.getElementById("id").value;
   var name = document.getElementById("name").value;
-  // var avatar = document.getElementById("img").value;
   var price = document.getElementById("price").value;
   var desc = document.getElementById("desc").value;
+  var promotion = document.getElementById("promotion").value;
+  var img=document.getElementById("fileInput").value;
+let image = img.split("\\")[2];
   var OneProduct = {
       id: id,
       name: name,
-      // avatar: avatar,
+      img: image,
       price: price,
       desc: desc,
-
+      promotion:promotion,
   }
   console.log(OneProduct);
   axios.put(`https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products/${id}`, OneProduct)
