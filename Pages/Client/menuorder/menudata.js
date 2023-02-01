d321e99a3936034d3dafae9d43d41821b814402a
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-02-01 22:07:46 +0700

    them gio hang

--- Pages/Client/menuorder/menudata.js
+++ Pages/Client/menuorder/menudata.js
@@ -83,4 +83,26 @@ function show() {
 };
   show();
 
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("cart");
+var close = document.getElementsByClassName("close")[0];
+var close_footer = document.getElementsByClassName("close-footer")[0];
+var order = document.getElementsByClassName("order")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+close.onclick = function () {
+  modal.style.display = "none";
+}
+close_footer.onclick = function () {
+  modal.style.display = "none";
+}
+order.onclick = function () {
+  alert("Cảm ơn bạn đã thanh toán đơn hàng")
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
