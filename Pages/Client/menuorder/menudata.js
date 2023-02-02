2274b58ea10aa9cafced80a70abba9ac725b3c43
TranQuocHuu03 <huu.tran24@Student.passerellesnumeriques.org>
2023-02-03 04:16:28 +0700

    thanh toán

--- Pages/Client/menuorder/menudata.js
+++ Pages/Client/menuorder/menudata.js
@@ -28,7 +28,7 @@ function show() {
                     menu1 +=`<div class="p-2 "><span class="text-danger " >-`+Product[i].promotion+`%</span></div> `,
                     menu1 +='<div class="p-2 "><span class="text-dark "><del>$'+parseInt(Product[i].price.slice(1))+'</del></span> </div>',
                     menu1 +=`<div class="p-2 "><span class="text-primary ">$`+newPrice+`</span></div> `
-                    menu1 +=`<button class="border-0 ml-2 bg-white"  onclick="basket(${Product[i].id})">
+                    menu1 +=`<button class="border-0 ml-2 bg-white"  onclick="basket (${Product[i].id})">
                     <span class="material-symbols-outlined">
                      add_shopping_cart
                     </span>
@@ -120,6 +120,8 @@ function show() {
   show();
 
   function basket(id) {
+    
+
     const sendPostRequest = async () => {
       try {
           await axios.get(`https://63aa9d5d7d7edb3ae62c2f74.mockapi.io/Products/${id}`)
@@ -131,6 +133,34 @@ function show() {
       .catch(function (error) {
         console.log(error);
       });
+
+      axios.get('https://63aa9ceffdc006ba6046faf6.mockapi.io/api/12/ORDER')
+  .then(function(response) {
+    var a=response.data;
+    var b=0;
+   
+    if (a.length==0){
+      b=parseInt(parseInt(Product.price.slice(1))-parseInt(Product.price.slice(1))*Product.promotion/100)
+      
+    }
+    
+    //   for(var i in a){
+    //   b+=parseInt(parseInt(a[i].price.slice(1))-parseInt(a[i].price.slice(1))*a[i].promotion/100)
+    // }
+   
+    console.log(b)
+    for(var i in a){
+      b+=parseInt(parseInt(a[i].price.slice(1))-parseInt(a[i].price.slice(1))*a[i].promotion/100)
+    }
+    console.log(a,'abc');
+    document.getElementById("productorder").innerHTML = b;
+    
+  })
+
+  const showOrder=()=> {
+    
+  }
+
       setTimeout(() => {
         a()
       }, 2000);
@@ -153,7 +183,7 @@ function show() {
           for (var i in product) {
             html += '<td scope="col">' + product[i].name + "</td>";
             html += '<td scope="col"> <img src="' + product[i].avatar + '" width="45px" height="45px"></td>';
-            html += '<td scope="col">' + product[i].price + "</td>";
+            html += '<td scope="col">' +parseInt(parseInt(product[i].price.slice(1))-parseInt(product[i].price.slice(1))*product[i].promotion/100) + "</td>";
             html += '<td scope="col">' + ` <button class="btn btn-danger" type="button"  onclick="deletes(${product[i].id})">delete</button>`+ "</td>";
             html += "</tr>";
           }
@@ -169,8 +199,18 @@ function show() {
           .catch(function (error) {
             console.log(error);
           });
-    
-    
+    // click xóa trừ giá
+    axios.get('https://63aa9ceffdc006ba6046faf6.mockapi.io/api/12/ORDER')
+    .then(function(response) {
+      var a=response.data;
+      var b=0;
+      for(var i in a){
+        b+=parseInt(parseInt(a[i].price.slice(1))-parseInt(a[i].price.slice(1))*a[i].promotion/100)
+      }
+      console.log(b,'abc');
+      document.getElementById("productorder").innerHTML = b;
+      
+    })
         
           alert("I'm sure!");
           a();
@@ -179,6 +219,12 @@ function show() {
       }
     }
 
+
+    // function order(){
+     
+    //   console.log('abc');
+    // }
+    
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("cart");
 var close = document.getElementsByClassName("close")[0];
@@ -193,7 +239,19 @@ close.onclick = function () {
 close_footer.onclick = function () {
   modal.style.display = "none";
 }
+
 order.onclick = function () {
+  // axios.get('https://63aa9ceffdc006ba6046faf6.mockapi.io/api/12/ORDER')
+  // .then(function(response) {
+  //   var a=response.data;
+  //   var b=0;
+  //   for(var i in a){
+  //     b+=parseInt(parseInt(a[i].price.slice(1))-parseInt(a[i].price.slice(1))*a[i].promotion/100)
+  //   }
+  //   console.log(b,'abc');
+  //   document.getElementById("productorder").innerHTML = b;
+    
+  // })
   alert("Cảm ơn bạn đã thanh toán đơn hàng")
 }
 window.onclick = function (event) {
